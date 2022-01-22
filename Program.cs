bc9ef78a39468c4aab89227875e262274388c5db
islamshireliyev <islamshireliyev99@gmail.com>
2022-01-22 15:13:58 +0400

    aaa

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+int a = 5;
+int b = 10;
+int c = 2;
+int d = 3;
+int e = a + b;
+Console.WriteLine(e);
+int f = c * d;
+Console.WriteLine(f);
+int g = e / f;
+Console.WriteLine(g);
+
+int j = e % f;
+Console.WriteLine(j);
+*/
+
+
+/*
+// ededin tek veya cut olmasinin yoxlama alqoritmi
+int a = 73;
+
+int p = a % 2;
+if (p==0)
+{
+    Console.WriteLine("cutdur");
+}
+else
+{
+    Console.WriteLine("tekdir");
+
+}
+
+*/
+
+
+/*
+
+// ededin 4-e bolunub bolunmemesi alqoritmi
+        int a = 17;
+        int s = a / 4;
+        if (s == 0)
+        {
+            Console.WriteLine("bolunur");
+
+        }
+        else
+        {
+            Console.WriteLine("bolunmur");
+        }
+
+        
+*/
+
+
+//.1-50 arasi (1 ve 50 de daxil olmaqla [1,50]) butun tek ededleri cap etmek
+
+
+
+/*
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        for (int i = 1; i <= 50;i++)
+           
+
+
+       {
+
+            if (i % 2 == 0)
+                continue;
+            Console.WriteLine(i);
+        }
+
+
+    }
+}
+
+       */
+
+
+
+/*
+//.1-50 arasi ancaq 5-e ya da 3-e bolunen ededleri cap etmek
+
+for (int i = 1; i <=50;i++)
+{
+   if(i%3==0 || i%5== 0)
+    
+        if(i%3==0 ||i%5==0)
+              
+    Console.WriteLine(i);
+
+}
+*/
+
+
+
+
+
+
+
